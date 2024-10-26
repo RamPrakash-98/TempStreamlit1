7e8ee0389b08f87b6af5ba7cbae75f901d484f20
Ram Prakash <75033908+RamPrakash-98@users.noreply.github.com>
2024-10-26 17:42:37 +0530

    Update addChart2.py

--- addChart2.py
+++ addChart2.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly_express as px
-from plotly.subplots import make_subplots
+# import plotly_express as px
+# from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 from random import randint,random
